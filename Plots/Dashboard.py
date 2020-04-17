565cc07c1df3274da04e789837bf167a9aab18d6
Virgil English <kenglis8@uncc.edu>
2020-04-17 19:05:35 -0400

    Dashboard dynamic

--- Plots/Dashboard.py
+++ Plots/Dashboard.py
@@ -5,8 +5,10 @@ from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.graph_objs as go
 
-def generate_dash(bar_fig, bar_desc, stack_fig, stack_desc, ling_fig, line_desc, multi_fig, multi_desc, heat_fig,
-                  heat_desc, bubble_fig, bubble_desc):
+def generate_dash(bar_fig, bar_desc, stack_fig, stack_desc, line_fig, line_desc, multi_fig, multi_desc, heat_fig,
+                  heat_desc, bubble_fig, bubble_desc, dashboard_title):
+    app = dash.Dash()
+
     # Layout
     app.layout = html.Div(children=[
         html.H1(children='Python Dash',
@@ -16,110 +18,55 @@ def generate_dash(bar_fig, bar_desc, stack_fig, stack_desc, ling_fig, line_desc,
                 }
                 ),
         html.Div('Web dashboard for Data Visualization using Python', style={'textAlign': 'center'}),
-        html.Div('Coronavirus COVID-19 Global Cases -  1/22/2020 to 3/17/2020', style={'textAlign': 'center'}),
+        html.Div(dashboard_title, style={'textAlign': 'center'}),
         html.Br(),
-        html.Br(),
-        html.Hr(style={'color': '#7FDBFF'}),
-        html.H3('Interactive Bar chart', style={'color': '#df1e56'}),
-        html.Div('This bar chart represent the number of confirmed cases in the first 20 countries of selected continent.'),
-        dcc.Graph(id='graph1'),
-        html.Div('Please select a continent', style={'color': '#ef3e18', 'margin':'10px'}),
-        dcc.Dropdown(
-            id='select-continent',
-            options=[
-                {'label': 'Asia', 'value': 'Asia'},
-                {'label': 'Africa', 'value': 'Africa'},
-                {'label': 'Europe', 'value': 'Europe'},
-                {'label': 'North America', 'value': 'North America'},
-                {'label': 'Oceania', 'value': 'Oceania'},
-                {'label': 'South America', 'value': 'South America'}
-            ],
-            value='Europe'
-        ),
         html.Br(),
         html.Hr(style={'color': '#7FDBFF'}),
         html.H3('Bar chart', style={'color': '#df1e56'}),
-        html.Div('This bar chart represent the number of confirmed cases in the first 20 states of the US.'),
+        html.Div(bar_desc),
         dcc.Graph(id='graph2',
-                  figure={
-                      'data': data_barchart,
-                      'layout': go.Layout(title='Corona Virus Confirmed Cases in The US',
-                                          xaxis={'title': 'States'}, yaxis={'title': 'Number of confirmed cases'})
-                  }
-                  ),
+                  figure=bar_fig),
         html.Hr(style={'color': '#7FDBFF'}),
         html.H3('Stack bar chart', style={'color': '#df1e56'}),
-        html.Div(
-            'This stack bar chart represent the CoronaVirus deaths, recovered and under treatment of all reported first 20 countries except China.'),
+        html.Div(stack_desc),
         dcc.Graph(id='graph3',
-                  figure={
-                      'data': data_stackbarchart,
-                      'layout': go.Layout(title='Corona Virus Cases in the first 20 country expect China',
-                                          xaxis={'title': 'Country'}, yaxis={'title': 'Number of cases'},
-                                          barmode='stack')
-                  }
-                  ),
+                  figure=stack_fig),
         html.Hr(style={'color': '#7FDBFF'}),
         html.H3('Line chart', style={'color': '#df1e56'}),
-        html.Div('This line chart represent the Corona Virus confirmed cases of all reported cases in the given period.'),
+        html.Div(line_desc),
         dcc.Graph(id='graph4',
-                  figure={
-                      'data': data_linechart,
-                      'layout': go.Layout(title='Corona Virus Confirmed Cases From 2020-01-22 to 2020-03-17',
-                                          xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
-                  }
-                  ),
+                  figure=line_fig),
         html.Hr(style={'color': '#7FDBFF'}),
         html.H3('Multi Line chart', style={'color': '#df1e56'}),
-        html.Div(
-            'This line chart represent the CoronaVirus death, recovered and under treatment cases of all reported cases in the given period.'),
+        html.Div(multi_desc),
         dcc.Graph(id='graph5',
-                  figure={
-                      'data': data_multiline,
-                      'layout': go.Layout(
-                          title='Corona Virus Death, Recovered and under treatment Cases From 2020-01-22 to 2020-03-17',
-                          xaxis={'title': 'Date'}, yaxis={'title': 'Number of cases'})
-                  }
-                  ),
+                  figure=multi_fig),
         html.Hr(style={'color': '#7FDBFF'}),
         html.H3('Bubble chart', style={'color': '#df1e56'}),
-        html.Div(
-            'This bubble chart represent the Corona Virus recovered and under treatment of all reported countries except China.'),
+        html.Div(bubble_desc),
         dcc.Graph(id='graph6',
-                  figure={
-                      'data': data_bubblechart,
-                      'layout': go.Layout(title='Corona Virus Confirmed Cases',
-                                          xaxis={'title': 'Recovered Cases'}, yaxis={'title': 'under Treatment Cases'},
-                                          hovermode='closest')
-                  }
-                  ),
+                  figure=bubble_fig),
         html.Hr(style={'color': '#7FDBFF'}),
         html.H3('Heat map', style={'color': '#df1e56'}),
-        html.Div(
-            'This heat map represent the Corona Virus recovered cases of all reported cases per day of week and week of month.'),
+        html.Div(heat_desc),
         dcc.Graph(id='graph7',
-                  figure={
-                      'data': data_heatmap,
-                      'layout': go.Layout(title='Corona Virus Recovered Cases',
-                                          xaxis={'title': 'Day of Week'}, yaxis={'title': 'Week of Month'})
-                  }
-                  )
+                  figure=heat_fig)
     ])
 
 
-    @app.callback(Output('graph1', 'figure'),
-                  [Input('select-continent', 'value')])
-    def update_figure(selected_continent):
-        filtered_df = df1[df1['Continent'] == selected_continent]
-
-        filtered_df = filtered_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-        new_df = filtered_df.groupby(['Country'])['Confirmed'].sum().reset_index()
-        new_df = new_df.sort_values(by=['Confirmed'], ascending=[False]).head(20)
-        data_interactive_barchart = [go.Bar(x=new_df['Country'], y=new_df['Confirmed'])]
-        return {'data': data_interactive_barchart, 'layout': go.Layout(title='Corona Virus Confirmed Cases in '+selected_continent,
-                                                                       xaxis={'title': 'Country'},
-                                                                       yaxis={'title': 'Number of confirmed cases'})}
-
+    # @app.callback(Output('graph1', 'figure'),
+    #               [Input('select-continent', 'value')])
+    # def update_figure(selected_continent):
+    #     filtered_df = df1[df1['Continent'] == selected_continent]
+    #
+    #     filtered_df = filtered_df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    #     new_df = filtered_df.groupby(['Country'])['Confirmed'].sum().reset_index()
+    #     new_df = new_df.sort_values(by=['Confirmed'], ascending=[False]).head(20)
+    #     data_interactive_barchart = [go.Bar(x=new_df['Country'], y=new_df['Confirmed'])]
+    #     return {'data': data_interactive_barchart, 'layout': go.Layout(title='Corona Virus Confirmed Cases in '+selected_continent,
+    #                                                                    xaxis={'title': 'Country'},
+    #                                                                    yaxis={'title': 'Number of confirmed cases'})}
+    #
 
     if __name__ == '__main__':
         app.run_server()
--- Plots/RequestHandler.py
+++ Plots/RequestHandler.py
@@ -197,6 +197,7 @@ def request_dash(dash_data):
     multi_fig, multi_desc = request_multi(dash_data['multi'], 1)
     bubble_fig, bubble_desc = request_bubble(dash_data['bubble'], 1)
     heat_fig, heat_desc = request_heat(dash_data['heat'], 1)
+    dash_title = dash_data['dash_title']
 
     d.generate_dash(bar_fig, bar_desc, stack_fig, stack_desc, line_fig, line_desc,
-                    multi_fig, multi_desc, heat_fig, heat_desc, bubble_fig, bubble_desc)
+                    multi_fig, multi_desc, heat_fig, heat_desc, bubble_fig, bubble_desc, dash_title)
