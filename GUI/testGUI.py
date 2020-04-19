f96c809e434fe323ab89b9bc2b8f595c8dc15cec
dhedgepeth <dnh1017@gmail.com>
2020-04-19 11:28:17 -0400

    Updated heat_map()
    Added option for user to calculate sum or mean and to add a limit.

--- GUI/testGUI.py
+++ GUI/testGUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import Plots.RequestHandler as request
 
 
 # Function used to clear the window to prepare it for a new page
@@ -131,6 +132,24 @@ def heat_map():
     z_entry = tk.Entry(body_frame)
     z_entry.place(relx=0.22, rely=0.58, relwidth=0.3)
 
+    # Create a drop down menu
+    option.set("Please make a selection")  # sets the default text on the menu selection
+    chart_selection = tk.OptionMenu(body_frame, option, "None", "Sum", "Mean")
+    chart_selection.place(relx=0.65, rely=0.58, relwidth=0.25)
+
+    limit_label = tk.Label(body_frame, bg='gray', text="Would you like to place a limit?")
+    limit_label.place(relx=0.01, rely=0.75)
+
+    limit_menu.set("Please make a selection")  # sets the default text on the menu selection
+    chart_selection = tk.OptionMenu(body_frame, limit_menu, "Yes", "No")
+    chart_selection.place(relx=0.3, rely=0.75, relwidth=0.25)
+
+    value_label = tk.Label(body_frame, bg='gray', text="Enter a limit, or 0 for none")
+    value_label.place(relx=0.6, rely=0.75)
+
+    limit_entry = tk.Entry(body_frame)
+    limit_entry.place(relx=0.84, rely=0.75)
+
     # Create a submit button to send all user entries to a variable
     # The parameters are each of the entries returning their user input
     submit_button = tk.Button(background, text="Submit", command=lambda: get_heat_map(file_path_entry.get(),
@@ -139,8 +158,12 @@ def heat_map():
                                                                                       x_entry.get(),
                                                                                       y_title_entry.get(),
                                                                                       y_entry.get(),
-                                                                                      z_entry.get()))
-    submit_button.place(relx=0.91, rely=0.92)
+                                                                                      z_entry.get(),
+                                                                                      option.get(),
+                                                                                      limit_menu.get(),
+                                                                                      limit_entry.get(),
+                                                                                      "Heat Map"))
+    submit_button.place(relx=0.93, rely=0.92)
 
 
 def line_chart():
@@ -385,7 +408,7 @@ def get_chart():
 
     elif option.get() == "Stacked Bar Chart":
         stacked_bar()
-
+    # This else statement would indicate that the user did not select a graph from the drop down menu
     else:
         # Create an error box pop up to alert user to select a graph to continue
         error_box = tk.Toplevel(width=300, height=150, bg='white')
@@ -406,9 +429,11 @@ def get_chart():
         button.place(relx=0.5, rely=0.5, anchor='n')
 
 
-# This method gets the data from the heat map entries to populate the dictionary that can be handled by the
+# This method gets the data from the heat map entry widgets to populate the dictionary that can be handled by the
 # RequestHandler.py
-def get_heat_map(file, chart_title, x_title, x, y_title, y, z):
+# The chart key should be different depending on which chart calls this method so that the correct method from the
+# RequestHandler.py can be called
+def get_heat_map(file, chart_title, x_title, x, y_title, y, z, sum_mean, limit_option, limit_value, chart_key):
     chart_data['file'] = file
     chart_data['title'] = chart_title
     chart_data['x_title'] = x_title
@@ -416,11 +441,23 @@ def get_heat_map(file, chart_title, x_title, x, y_title, y, z):
     chart_data['y_title'] = y_title
     chart_data['y'] = y
     chart_data['z'] = z
-    print(chart_data)
+    chart_data['limit_num'] = limit_value
+
+    if sum_mean == "Sum":
+        chart_data['sum'] = 1
+    elif sum_mean == "Mean":
+        chart_data['mean'] = 1
+
+    if limit_option == "Yes":
+        chart_data['limit'] = 1
+
+    if chart_key == "Heat Map":
+        request.request_heat(chart_data, 0)
 
 
-# Create a global dictionary
-chart_data = {'file': None, 'title': None, 'x_title': None, 'x': None, 'y_title': None, 'y': None, 'z': None}
+# Create a global dictionary that can be sent to RequestHandler.py
+chart_data = {'file': None, 'title': None, 'x_title': None, 'x': None, 'y_title': None, 'y': None, 'z': None,
+              'sum': 0, 'mean': 0, 'limit': 0, 'limit_num': 0}
 
 # Create a new window
 window = tk.Tk()
@@ -430,6 +467,7 @@ window.title("Project Carrot")
 
 # Creates a tkinter variable to use in functions
 option = tk.StringVar(window)
+limit_menu = tk.StringVar(window)
 
 # Create a canvas to adjust the window size
 window_size = tk.Canvas(window, width=800, height=400)
@@ -440,6 +478,7 @@ background = tk.Frame(window, bg='white')
 # The background takes up the entire window, all widgets will be placed inside this frame
 background.place(relwidth=1, relheight=1)
 
-front_page()
+# front_page()
+heat_map()
 
 window.mainloop()
